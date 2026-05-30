caefce30c905164204e8ad45d674cfcef2a27b8c
Moritz Olthoff <moritz.olthoff@t-online.de>
2026-05-30 15:24:22 +0200

    fix hit recieve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
 import time
+
+from flask import Flask, request
+
 from data.device import Device
 from data.player import Player
 
@@ -11,23 +13,27 @@ startTime = round(time.time() * 1000)
 
 print(f"Started Server at {startTime} milliseconds after boot")
 
-def calculateTime(clientid, clienttime):
-	return devices.get(clientid).get_time() + clienttime
+
+def calculate_time(client_id, client_time):
+    return devices.get(client_id).get_time() + client_time
+
 
 @app.route("/api/test")
 def api_test():
     print("Anfrage erhalten!")
     return ("Anfrage erhalten!", 200)
 
+
 @app.post("/api/sync")
 def sync_device():
-    client_time = request.args.get('time')
+    client_time = int(request.args.get("time"))
     client_id = str(len(devices))
-    client_type = request.args.get('type')
+    client_type = request.args.get("type")
 
     device = Device(client_time, client_type)
 
     devices.update({client_id: device})
+    print(devices)
 
     if client_type == "Vest":
         players.update({client_id: Player(1)})
@@ -35,35 +41,38 @@ def sync_device():
 
     return (client_id, 200)
 
+
 @app.get("/api/data")
 def get_data():
-    device_id = request.args.get('id')
+    device_id = request.args.get("id")
 
-    device_time = devices.get(device_id).get_time()
+    device_time = int(devices.get(device_id).get_time())
     device_type = devices.get(device_id).get_type()
 
     response = f"ID: {device_id} </n>Time: {device_time} </n>Type: {device_type}"
 
     return (response, 200)
 
+
 @app.post("/api/hit/send")
 def send_hit():
-    client_id = request.args.get('id')
-    client_time = request.args.get('time')
+    client_id = request.args.get("id")
+    client_time = int(request.args.get("time"))
 
-    gametime = calculateTime(client_id, client_time)
+    gametime = calculate_time(client_id, client_time)
     print(f"ID: {client_id} schießt bei {gametime} mills")
 
     response = f"Du hast ID: {client_id} und TIME: {client_time} (Clienttime) als Schuss gesendet"
     return (response, 200)
 
+
 @app.post("/api/hit/recieve")
 def recieve_hit():
-    client_id = request.args.get('id')
-    hitter_id = request.args.get('their_id')
-    client_time = request.args.get('time')
+    client_id = request.args.get("my_id")
+    hitter_id = request.args.get("their_id")
+    client_time = int(request.args.get("time"))
 
-    gametime = calculateTime(client_id, client_time)
+    gametime = calculate_time(client_id, client_time)
     print(f"ID: {client_id} wurde von ID: {hitter_id} bei {gametime} mills getroffen")
 
     players.get(client_id).deal_damage(1)
