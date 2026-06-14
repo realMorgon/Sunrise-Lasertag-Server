480d0116062be3f5be2b5036b48e308cce7e14c5
Moritz Olthoff <moritz.olthoff@t-online.de>
2026-06-14 18:09:33 +0200

    Didn't save the uninstall.sh :skull:

--- uninstall.sh
+++ uninstall.sh
@@ -15,12 +15,21 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 export $(grep -v '^#' "$SCRIPT_DIR/.env" | xargs)
 
 # TODO: Kill running processes
+sudo systemctl stop hostapd dnsmasq
+sudo systemctl disable hostapd dnsmasq
+
+sudo apt-get remove --purge -y hostapd dnsmasq
+sudo apt-get autoremove -y
 
 # Remove PostgreSQL database and user
 sudo -u postgres psql -c "DROP DATABASE $DB_NAME;"
 sudo -u postgres psql -c "DROP USER $DB_USER;"
 
-# TODO: Close Hotspot
+sudo systemctl stop postgresql
+sudo systemctl disable postgresql
+
+sudo apt-get remove --purge -y postgresql
+sudo apt-get autoremove -y
 
 # Remove .env file
 rm -f "$SCRIPT_DIR/.env"
