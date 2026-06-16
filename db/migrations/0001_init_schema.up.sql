24da4c936884315755a9e32584df81bd61f7a623
Moritz Olthoff <moritz.olthoff@t-online.de>
2026-06-17 00:32:07 +0200

    Enhance Migration 0001

--- db/migrations/0001_init_schema.up.sql
+++ db/migrations/0001_init_schema.up.sql
@@ -2,36 +2,47 @@
 CREATE SCHEMA data;
 CREATE SCHEMA matches;
 
+-- Enums
+CREATE TYPE data.device_type AS ENUM ('phaser', 'vest', 'grenade', 'stim');
+CREATE TYPE matches.match_type AS ENUM ('deathmatch', 'team_deathmatch', 'capture_the_flag');
+CREATE TYPE matches.event_type AS ENUM ('shot', 'hit', 'heal', 'revive');
+
 -- Creating data tables
 CREATE TABLE data.players(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE data.devices(
     id UUID PRIMARY KEY,
-    owner_id UUID REFERENCES players(id),
-    device_type VARCHAR(255) NOT NULL,
+    owner_id UUID REFERENCES data.players(id),
+    device_type data.device_type NOT NULL
 );
 
 -- Creating matches tables
 CREATE TABLE matches.matches(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    started_at BIGINT NOT NULL,
+    ended_at BIGINT,
+    winner_id UUID REFERENCES data.players(id),
+    match_type matches.match_type NOT NULL,
+    match_name VARCHAR(60) NOT NULL,
+    winner_team VARCHAR(60)
 );
 
 CREATE TABLE matches.players(
-    match_id UUID NOT NULL REFERENCES matches(id),
-    player_id UUID NOT NULL REFERENCES players(id),
-    team VARCHAR(63),
+    match_id UUID NOT NULL REFERENCES matches.matches(id),
+    player_id UUID NOT NULL REFERENCES data.players(id),
+    team VARCHAR(60),
     PRIMARY KEY (match_id, player_id)
 );
 
 CREATE TABLE matches.events(
-    match_id UUID NOT NULL REFERENCES matches(id),
+    match_id UUID NOT NULL REFERENCES matches.matches(id),
     event_time BIGINT NOT NULL,
-    event_type VARCHAR(255) NOT NULL,
-    active_device_id UUID NOT NULL REFERENCES devices(id),
-    passive_device_id UUID REFERENCES devices(id),
+    active_device_id UUID NOT NULL REFERENCES data.devices(id),
+    passive_device_id UUID REFERENCES data.devices(id),
+    event_type matches.event_type NOT NULL,
 
     PRIMARY KEY (match_id, event_time, active_device_id)
 );
